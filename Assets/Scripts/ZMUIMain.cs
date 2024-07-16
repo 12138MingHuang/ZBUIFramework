3adcf0ddae320e82cdd77359f6a123af89536c27
张斌 <bin.zhang@appshahe.com>
2024-07-16 17:13:50 +0800

    add：遮罩测试

--- Assets/Scripts/ZMUIMain.cs
+++ Assets/Scripts/ZMUIMain.cs
@@ -6,6 +6,7 @@ public class ZMUIMain : MonoBehaviour
 {
     private void Awake()
     {
+        UIModule.Instance.Initialize();
         LoginWindow loginWindow = UIModule.Instance.PopUpWindow<LoginWindow>();
         loginWindow.Test();
 
@@ -16,7 +17,6 @@ public class ZMUIMain : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            UIModule.Instance.Initialize();
             UIModule.Instance.PopUpWindow<LoginWindow>();
         }
 
@@ -33,6 +33,31 @@ public class ZMUIMain : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.T))
         {
             UIModule.Instance.DestroyAllWindow();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Debug.Log("是否启用单遮模式：" + UISetting.Instance.SINGMASK_SYSTEM);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            UIModule.Instance.PopUpWindow<AccountWindow>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            UIModule.Instance.PopUpWindow<SignUpWindow>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            UIModule.Instance.HideWindow<AccountWindow>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            UIModule.Instance.HideWindow<SignUpWindow>();
         }
     }
 }
